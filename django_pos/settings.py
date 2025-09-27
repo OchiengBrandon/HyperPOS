1d207f0b66aff277b3cbd7363ecf1f1db48367ca
Brandon <brandonochieng72@gmail.com>
2025-09-27 12:50:17 +0300

    Fix context processor import error
    - Changed 'pos_app.context_processors.business_context' to 'pos_app.context_processors.business_settings'
    - The function name in context_processors.py is business_settings, not business_context
    - Fixes ImportError: Module 'pos_app.context_processors' does not define a 'business_context' attribute/class

--- django_pos/settings.py
+++ django_pos/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'pos_app.context_processors.business_context',
+                'pos_app.context_processors.business_settings',
             ],
         },
     },
